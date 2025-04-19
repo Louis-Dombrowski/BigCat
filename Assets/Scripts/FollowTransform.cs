4804fa9abde4ed9273afc3cac14c0880a3d14674
Louis <louis.t.dombrowski@gmail.com>
2025-04-19 17:03:26 -0400

    Work on puppeting the cat rig

--- Assets/Scripts/FollowTransform.cs
+++ Assets/Scripts/FollowTransform.cs
@@ -5,11 +5,13 @@ public class FollowTransform : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private Vector3 posOffset;
-    [SerializeField] private Quaternion rotOffset;
+    [SerializeField] private Vector3 rotOffset;
+    [SerializeField] private bool followRot = true;
+    [SerializeField] private bool followPos = true;
     
     private void LateUpdate()
     {
-        transform.position = target.position;
-        transform.rotation = target.rotation;
+        if (followPos) transform.position = target.position + posOffset;
+        if (followRot) transform.rotation = target.rotation * Quaternion.Euler(rotOffset);
     }
 }
